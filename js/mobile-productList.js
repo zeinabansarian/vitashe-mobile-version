ce05ad37b82a10d5ed1c1ac17ca187c056530707
zeinabansarian <zeinabansarian6280@gmail.com>
2024-01-07 20:31:24 +0330

    fix bugs

--- js/mobile-productList.js
+++ js/mobile-productList.js
@@ -1,16 +1,21 @@
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: "auto",
-  loop: true,
-  paginationClickable: true,
+//   loop: true,
+  controller: {
+    control: swiperDetails,
+  },
+//   paginationClickable: true,
   spaceBetween: 0,
 });
 
 var swiperDetails = new Swiper(".swiperDetails", {
-  slidesPerView: 1,
-  centeredSlides: true,
-
-  loop: true,
-  spaceBetween: 0
+    slidesPerView:1,
+    // loop: true
+    controller: {
+        control: swiper,
+      },
+    paginationClickable: true,
+    spaceBetween: 0,
 });
 swiper.controller.control = swiperDetails;
 swiperDetails.controller.control = swiper;
